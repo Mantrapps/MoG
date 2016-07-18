7f8de2097cd72c2053221405a598759a00e26ec3
Kai Zhu <Kevin.zhu.cn@outlook.com>
2016-07-18 00:23:10 -0500

    1.0
    Initiation

---
+++ main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  MoG
+//
+//  Created by Kaiz on 7/1/16.
+//  Copyright © 2016 CS6364. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
